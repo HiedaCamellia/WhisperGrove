ac003eccc46f3a9d9d41a7587fff7d484c327049
Yuqi154 <1123489751@qq.com>
2024-08-06 12:50:15 +0800

    Revert "内置camellialib"
    This reverts commit d3a9b70cb9a714f3b97889816fec476f1fbb7d8d.

--- src/main/java/org/hiedacamellia/whispergrove/Config.java
+++ src/main/java/org/hiedacamellia/whispergrove/Config.java
@@ -1,5 +1,9 @@
 package org.hiedacamellia.whispergrove;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
@@ -7,41 +11,32 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.config.ModConfigEvent;
 import net.neoforged.neoforge.common.ModConfigSpec;
-import org.hiedacamellia.camellialib.CamelliaLib;
-import org.hiedacamellia.camellialib.core.entry.common.CamelliaConfigBuilder;
-
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import static net.neoforged.fml.config.ModConfig.Type.COMMON;
 
 // An example config class. This is not required, but it's a good idea to have one to keep your config organized.
 // Demonstrates how to use Neo's config APIs
 @EventBusSubscriber(modid = WhisperGrove.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class Config
 {
-    public static final CamelliaConfigBuilder builder = CamelliaConfigBuilder.create().modId(WhisperGrove.MODID).side(COMMON);
-    //private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+    private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
-    private static final ModConfigSpec.BooleanValue LOG_DIRT_BLOCK = builder
+    private static final ModConfigSpec.BooleanValue LOG_DIRT_BLOCK = BUILDER
             .comment("Whether to log the dirt block on common setup")
             .define("logDirtBlock", true);
 
-    private static final ModConfigSpec.IntValue MAGIC_NUMBER = builder
+    private static final ModConfigSpec.IntValue MAGIC_NUMBER = BUILDER
             .comment("A magic number")
             .defineInRange("magicNumber", 42, 0, Integer.MAX_VALUE);
 
-    public static final ModConfigSpec.ConfigValue<String> MAGIC_NUMBER_INTRODUCTION = builder
+    public static final ModConfigSpec.ConfigValue<String> MAGIC_NUMBER_INTRODUCTION = BUILDER
             .comment("What you want the introduction message to be for the magic number")
             .define("magicNumberIntroduction", "The magic number is... ");
 
     // a list of strings that are treated as resource locations for items
-    private static final ModConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = builder
+    private static final ModConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER
             .comment("A list of items to log on common setup.")
             .define("items", List.of("minecraft:iron_ingot"), Config::validateItemName);
 
-    static final ModConfigSpec SPEC = builder.build();
+    static final ModConfigSpec SPEC = BUILDER.build();
 
     public static boolean logDirtBlock;
     public static int magicNumber;
