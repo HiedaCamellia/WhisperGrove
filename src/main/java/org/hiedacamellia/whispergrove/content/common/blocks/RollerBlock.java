76635fa122ef4907cb8e4033e09e32bf4db849cc
Yuqi154 <1123489751@qq.com>
2024-08-13 14:21:55 +0800

    修复模型贴图错误

--- src/main/java/org/hiedacamellia/whispergrove/content/common/blocks/RollerBlock.java
+++ src/main/java/org/hiedacamellia/whispergrove/content/common/blocks/RollerBlock.java
@@ -1,9 +1,26 @@
 package org.hiedacamellia.whispergrove.content.common.blocks;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.hiedacamellia.whispergrove.core.entry.BaseBlock;
+import org.hiedacamellia.whispergrove.core.recipe.generalherbprocess.GeneralHerbProcessApplier;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class RollerBlock extends BaseBlock {
     public RollerBlock(String regname) {
@@ -15,4 +32,17 @@ public class RollerBlock extends BaseBlock {
                 .noOcclusion(),
                 regname);
     }
+
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+        return Shapes.join(box(4, 0, 0, 12, 6, 16), box(7, 4, 10, 9, 10, 16).move(0,0,0), BooleanOp.OR);
+    }
+
+    @Override
+    public ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
+        GeneralHerbProcessApplier.apply(state, List.of(stack), level, pos);
+        return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
+    }
+
 }
