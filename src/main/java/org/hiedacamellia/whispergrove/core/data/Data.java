2f229fbe176def377a121e232d9cfee4f41cb812
glyceryl <494192175@qq.com>
2024-08-18 16:20:43 +0800

    修复dg问题

--- src/main/java/org/hiedacamellia/whispergrove/core/data/Data.java
+++ src/main/java/org/hiedacamellia/whispergrove/core/data/Data.java
@@ -1,14 +1,20 @@
 package org.hiedacamellia.whispergrove.core.data;
 
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
+import org.hiedacamellia.whispergrove.WhisperGrove;
 import org.hiedacamellia.whispergrove.core.data.lang.ChineseLanguageProvider;
 import org.hiedacamellia.whispergrove.core.data.lang.ClassicalChineseLanguageProvider;
 import org.hiedacamellia.whispergrove.core.data.lang.EnglishLanguageProvider;
-import org.hiedacamellia.whispergrove.core.data.provider.ModelProvider;
-import org.hiedacamellia.whispergrove.core.data.provider.StateProvider;
+import org.hiedacamellia.whispergrove.core.data.provider.WGItemModelProvider;
+import org.hiedacamellia.whispergrove.core.data.provider.WGBlockStateProvider;
 import org.hiedacamellia.whispergrove.core.data.provider.WGRecipeProvider;
 
+@EventBusSubscriber(modid = WhisperGrove.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class Data {
+
+    @SubscribeEvent
     public static void onGatherData(GatherDataEvent event) {
         var gen = event.getGenerator();
         var packOutput = gen.getPackOutput();
@@ -16,8 +22,9 @@ public class Data {
         gen.addProvider(event.includeClient(), new EnglishLanguageProvider(packOutput));
         gen.addProvider(event.includeClient(), new ChineseLanguageProvider(packOutput));
         gen.addProvider(event.includeClient(), new ClassicalChineseLanguageProvider(packOutput));
-        gen.addProvider(event.includeClient(), new ModelProvider(packOutput, helper));
-        gen.addProvider(event.includeClient(), new StateProvider(packOutput, helper));
+        gen.addProvider(event.includeClient(), new WGItemModelProvider(packOutput, helper));
+        gen.addProvider(event.includeClient(), new WGBlockStateProvider(packOutput, helper));
         gen.addProvider(event.includeServer(), new WGRecipeProvider(packOutput, event.getLookupProvider()));
     }
+
 }
