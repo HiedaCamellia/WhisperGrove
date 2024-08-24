8fb403b685531dc55da8386e2ddb45e6ece4279b
HaooooZhang <haoooo.zhang@outlook.com>
2024-08-24 21:45:47 +0800

    把写康了的方块修了

--- src/main/java/org/hiedacamellia/whispergrove/registers/WGBlockItem.java
+++ src/main/java/org/hiedacamellia/whispergrove/registers/WGBlockItem.java
@@ -9,12 +9,12 @@ import org.hiedacamellia.whispergrove.WhisperGrove;
 public class WGBlockItem {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(WhisperGrove.MODID);
 
-    public static final DeferredItem<BlockItem> ROU_GUI_LEAVES = ITEMS.register("rou_gui_leaves", () -> new BlockItem(WGBlock.CASSIA_LEAVES.get(), new Item.Properties()));
-    public static final DeferredItem<BlockItem> ROU_GUI_LOG = ITEMS.register("rou_gui_log", () -> new BlockItem(WGBlock.CASSIA_LOG.get(), new Item.Properties()));
-    public static final DeferredItem<BlockItem> ROU_GUI_STRIPPED_LOG = ITEMS.register("rou_gui_stripped_log", () -> new BlockItem(WGBlock.CASSIA_STRIPPED_LOG.get(), new Item.Properties()));
-    public static final DeferredItem<BlockItem> ROU_GUI_PLANKS = ITEMS.register("rou_gui_planks", () -> new BlockItem(WGBlock.CASSIA_PLANKS.get(), new Item.Properties()));
-    public static final DeferredItem<BlockItem> ROU_GUI_SLAB = ITEMS.register("rou_gui_slab", () -> new BlockItem(WGBlock.CASSIA_SLAB.get(), new Item.Properties()));
-    public static final DeferredItem<BlockItem> ROU_GUI_STAIR = ITEMS.register("rou_gui_stair", () -> new BlockItem(WGBlock.CASSIA_STAIRS.get(), new Item.Properties()));
+    public static final DeferredItem<BlockItem> CASSIA_LEAVES = ITEMS.register("cassia_leaves", () -> new BlockItem(WGBlock.CASSIA_LEAVES.get(), new Item.Properties()));
+    public static final DeferredItem<BlockItem> CASSIA_LOG = ITEMS.register("cassia_log", () -> new BlockItem(WGBlock.CASSIA_LOG.get(), new Item.Properties()));
+    public static final DeferredItem<BlockItem> CASSIA_STRIPPED_LOG = ITEMS.register("cassia_stripped_log", () -> new BlockItem(WGBlock.CASSIA_STRIPPED_LOG.get(), new Item.Properties()));
+    public static final DeferredItem<BlockItem> CASSIA_PLANKS = ITEMS.register("cassia_planks", () -> new BlockItem(WGBlock.CASSIA_PLANKS.get(), new Item.Properties()));
+    public static final DeferredItem<BlockItem> CASSIA_SLAB = ITEMS.register("cassia_slab", () -> new BlockItem(WGBlock.CASSIA_SLAB.get(), new Item.Properties()));
+    public static final DeferredItem<BlockItem> CASSIA_STAIRS = ITEMS.register("cassia_stairs", () -> new BlockItem(WGBlock.CASSIA_STAIRS.get(), new Item.Properties()));
     public static final DeferredItem<BlockItem> ROLLER = ITEMS.register("roller", () -> new BlockItem(WGBlock.ROLLER.get(), new Item.Properties()));
     public static final DeferredItem<BlockItem> SPRINGING = ITEMS.register("springing", () -> new BlockItem(WGBlock.SPRINGING.get(), new Item.Properties()));
 
