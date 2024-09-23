b73d806558f1727cf0de532ffba91c617410c950
Yuqi154 <1123489751@qq.com>
2024-09-23 12:34:41 +0800

    添加野生药材dg

--- src/main/java/org/hiedacamellia/whispergrove/core/data/provider/WGDataPackProvider.java
+++ src/main/java/org/hiedacamellia/whispergrove/core/data/provider/WGDataPackProvider.java
@@ -31,26 +31,35 @@ public class WGDataPackProvider extends DatapackBuiltinEntriesProvider {
                                     WGFeature.CASSIA_TREE_KEY,
                                     WGFeature.CASSIA_TREE
                             );
+                            bootstrap.register(
+                                    WGFeature.WG_MED_KEY,
+                                    WGFeature.WG_MED
+                            );
                         }).add(Registries.PLACED_FEATURE, bootstrap -> {
                             bootstrap.register(
                                     WGFeature.CASSIA_TREE_PLACED_KEY,
                                     WGFeature.CASSIA_TREE_PLACED
                             );
+                            bootstrap.register(
+                                    WGFeature.WG_MED_PLACED_KEY,
+                                    WGFeature.WG_MED_PLACED
+                            );
                         }).add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, bootstrap -> {
-                            // Lookup any necessary registries.
-                            // Static registries only need to be looked up if you need to grab the tag data.
                             HolderGetter<Biome> biomes = bootstrap.lookup(Registries.BIOME);
                             HolderGetter<PlacedFeature> placedFeatures = bootstrap.lookup(Registries.PLACED_FEATURE);
 
-                            // Register the biome modifiers.
                             bootstrap.register(WGBiomeModifier.CASSIA_TREE,
                                     new BiomeModifiers.AddFeaturesBiomeModifier(
-                                            // The biome(s) to generate within
                                             HolderSet.direct(List.of(biomes.getOrThrow(Biomes.DARK_FOREST),biomes.getOrThrow(Biomes.FOREST),biomes.getOrThrow(Biomes.BIRCH_FOREST))),
-                                            // The feature(s) to generate within the biomes
                                             HolderSet.direct(placedFeatures.getOrThrow(WGFeature.CASSIA_TREE_PLACED_KEY)),
-                                            // The generation step
                                             GenerationStep.Decoration.SURFACE_STRUCTURES
+                                    )
+                            );
+                            bootstrap.register(WGBiomeModifier.CASSIA_TREE,
+                                    new BiomeModifiers.AddFeaturesBiomeModifier(
+                                            HolderSet.direct(List.of(biomes.getOrThrow(Biomes.DARK_FOREST),biomes.getOrThrow(Biomes.FOREST),biomes.getOrThrow(Biomes.BIRCH_FOREST))),
+                                            HolderSet.direct(placedFeatures.getOrThrow(WGFeature.WG_MED_PLACED_KEY)),
+                                            GenerationStep.Decoration.VEGETAL_DECORATION
                                     )
                             );
                         }),
--- src/main/java/org/hiedacamellia/whispergrove/registers/WGFeature.java
+++ src/main/java/org/hiedacamellia/whispergrove/registers/WGFeature.java
@@ -1,39 +1,32 @@
 package org.hiedacamellia.whispergrove.registers;
 
 import net.minecraft.core.Holder;
-import net.minecraft.core.Vec3i;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.util.random.SimpleWeightedRandomList;
-import net.minecraft.util.valueproviders.BiasedToBottomInt;
 import net.minecraft.util.valueproviders.ConstantInt;
-import net.minecraft.util.valueproviders.IntProvider;
-import net.minecraft.util.valueproviders.WeightedListInt;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.Heightmap;
-import net.minecraft.world.level.levelgen.VerticalAnchor;
-import net.minecraft.world.level.levelgen.blockpredicates.BlockPredicate;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.RandomPatchConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.SimpleBlockConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
+import net.minecraft.world.level.levelgen.feature.stateproviders.NoiseThresholdProvider;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
-import net.minecraft.world.level.levelgen.heightproviders.BiasedToBottomHeight;
-import net.minecraft.world.level.levelgen.heightproviders.ConstantHeight;
-import net.minecraft.world.level.levelgen.heightproviders.HeightProvider;
 import net.minecraft.world.level.levelgen.placement.*;
-import net.neoforged.neoforge.registries.DeferredHolder;
+import net.minecraft.world.level.levelgen.synth.NormalNoise;
 import org.hiedacamellia.whispergrove.WhisperGrove;
 
 import java.util.List;
 
 public class WGFeature {
     public static final ResourceKey<ConfiguredFeature<?,?>> CASSIA_TREE_KEY = ResourceKey.create(
-            Registries.CONFIGURED_FEATURE, // The registry this key is for
-            WhisperGrove.prefix("cassia_tree") // The registry name
+            Registries.CONFIGURED_FEATURE,
+            WhisperGrove.prefix("cassia_tree")
     );
 
     public static final ConfiguredFeature<TreeConfiguration, Feature<TreeConfiguration>> CASSIA_TREE = new ConfiguredFeature<>(Feature.TREE,new TreeConfiguration.TreeConfigurationBuilder(
@@ -45,19 +38,48 @@ public class WGFeature {
     ).forceDirt().build());
 
     public static final ResourceKey<PlacedFeature> CASSIA_TREE_PLACED_KEY = ResourceKey.create(
-            Registries.PLACED_FEATURE, // The registry this key is for
-            WhisperGrove.prefix("cassia_tree") // The registry name
+            Registries.PLACED_FEATURE,
+            WhisperGrove.prefix("cassia_tree")
     );
+
     public static final PlacedFeature CASSIA_TREE_PLACED = new PlacedFeature(Holder.direct(CASSIA_TREE),
             List.of(HeightmapPlacement.onHeightmap(Heightmap.Types.OCEAN_FLOOR),
-//                    HeightmapPlacement.onHeightmap(Heightmap.Types.MOTION_BLOCKING_NO_LEAVES),
-//                    HeightmapPlacement.onHeightmap(Heightmap.Types.WORLD_SURFACE),
                     InSquarePlacement.spread(),
                     CountPlacement.of(1),
                     BiomeFilter.biome(),
-                    //HeightRangePlacement.of(BiasedToBottomHeight.of(new VerticalAnchor.AboveBottom(50),new VerticalAnchor.BelowTop(100),10)),
                     SurfaceWaterDepthFilter.forMaxDepth(0),
                     PlacementUtils.filteredByBlockSurvival(Blocks.BIRCH_SAPLING)
-                    //BlockPredicateFilter.forPredicate(BlockPredicate.wouldSurvive(Blocks.DIRT.defaultBlockState(),Vec3i.ZERO.above()))
+            ));
+
+    public static final ResourceKey<ConfiguredFeature<?,?>> WG_MED_KEY = ResourceKey.create(
+            Registries.CONFIGURED_FEATURE,
+            WhisperGrove.prefix("wg_med")
+    );
+
+    public static final ConfiguredFeature<RandomPatchConfiguration, Feature<RandomPatchConfiguration>> WG_MED = new ConfiguredFeature<>(Feature.FLOWER,new RandomPatchConfiguration(
+            64, 6, 2,
+            PlacementUtils.onlyWhenEmpty(Feature.SIMPLE_BLOCK,
+                    new SimpleBlockConfiguration(
+                            new NoiseThresholdProvider(2345L,
+                                    new NormalNoise.NoiseParameters(0, 1.0), 0.005F, -0.8F, 0.33333334F,
+                                    Blocks.DANDELION.defaultBlockState(),
+                                    List.of(
+                                            WGBlock.CROP_BLOCKS.get("rehmannia").get().defaultBlockState(),
+                                            WGBlock.CROP_BLOCKS.get("licorice").get().defaultBlockState(),
+                                            WGBlock.CROP_BLOCKS.get("milkvetch").get().defaultBlockState(),
+                                            WGBlock.CROP_BLOCKS.get("gentian").get().defaultBlockState(),
+                                            WGBlock.CROP_BLOCKS.get("ginseng").get().defaultBlockState()
+                                    ), List.of())))
+    ));
+    public static final ResourceKey<PlacedFeature> WG_MED_PLACED_KEY = ResourceKey.create(
+            Registries.PLACED_FEATURE,
+            WhisperGrove.prefix("wg_med")
+    );
+
+    public static final PlacedFeature WG_MED_PLACED = new PlacedFeature(Holder.direct(WG_MED),
+            List.of(HeightmapPlacement.onHeightmap(Heightmap.Types.MOTION_BLOCKING),
+                    InSquarePlacement.spread(),
+                    BiomeFilter.biome(),
+                    RarityFilter.onAverageOnceEvery(20)
             ));
 }
