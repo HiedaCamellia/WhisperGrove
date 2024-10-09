35631f0ff3e7bf5052f5f092ed3ce0e1d3ea52f0
glyceryl <494192175@qq.com>
2024-10-09 17:31:59 +0800

    修复一个地物相关的问题。

--- src/main/java/org/hiedacamellia/whispergrove/core/data/provider/WGDataPackProvider.java
+++ src/main/java/org/hiedacamellia/whispergrove/core/data/provider/WGDataPackProvider.java
@@ -22,48 +22,25 @@ import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 public class WGDataPackProvider extends DatapackBuiltinEntriesProvider {
-    public WGDataPackProvider(PackOutput output,CompletableFuture<HolderLookup.Provider> lookupProvider) {
-        super(output,
-                lookupProvider,
-                new RegistrySetBuilder()
-                        .add(Registries.CONFIGURED_FEATURE, bootstrap -> {
-                            bootstrap.register(
-                                    WGFeature.CASSIA_TREE_KEY,
-                                    WGFeature.CASSIA_TREE
-                            );
-                            bootstrap.register(
-                                    WGFeature.WG_MED_KEY,
-                                    WGFeature.WG_MED
-                            );
-                        }).add(Registries.PLACED_FEATURE, bootstrap -> {
-                            bootstrap.register(
-                                    WGFeature.CASSIA_TREE_PLACED_KEY,
-                                    WGFeature.CASSIA_TREE_PLACED
-                            );
-                            bootstrap.register(
-                                    WGFeature.WG_MED_PLACED_KEY,
-                                    WGFeature.WG_MED_PLACED
-                            );
-                        }).add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, bootstrap -> {
-                            HolderGetter<Biome> biomes = bootstrap.lookup(Registries.BIOME);
-                            HolderGetter<PlacedFeature> placedFeatures = bootstrap.lookup(Registries.PLACED_FEATURE);
 
-                            bootstrap.register(WGBiomeModifier.CASSIA_TREE,
-                                    new BiomeModifiers.AddFeaturesBiomeModifier(
-                                            HolderSet.direct(List.of(biomes.getOrThrow(Biomes.DARK_FOREST),biomes.getOrThrow(Biomes.FOREST),biomes.getOrThrow(Biomes.BIRCH_FOREST))),
-                                            HolderSet.direct(placedFeatures.getOrThrow(WGFeature.CASSIA_TREE_PLACED_KEY)),
-                                            GenerationStep.Decoration.SURFACE_STRUCTURES
-                                    )
-                            );
-//                            bootstrap.register(WGBiomeModifier.WG_MED,
-//                                    new BiomeModifiers.AddFeaturesBiomeModifier(
-//                                            HolderSet.direct(List.of(biomes.getOrThrow(Biomes.DARK_FOREST),biomes.getOrThrow(Biomes.FOREST),biomes.getOrThrow(Biomes.BIRCH_FOREST))),
-//                                            HolderSet.direct(placedFeatures.getOrThrow(WGFeature.WG_MED_PLACED_KEY)),
-//                                            GenerationStep.Decoration.VEGETAL_DECORATION
-//                                    )
-//                            );
-                        }),
-                Set.of(WhisperGrove.MODID));
+    public WGDataPackProvider(PackOutput output,CompletableFuture<HolderLookup.Provider> lookupProvider) {
+        super(output, lookupProvider, new RegistrySetBuilder()
+            .add(Registries.CONFIGURED_FEATURE, bootstrap -> {
+                bootstrap.register(WGFeature.CASSIA_TREE_KEY, WGFeature.CASSIA_TREE);
+                bootstrap.register(WGFeature.WG_MED_KEY, WGFeature.WG_MED);
+            }).add(Registries.PLACED_FEATURE, bootstrap -> {
+                bootstrap.register(WGFeature.CASSIA_TREE_PLACED_KEY, WGFeature.CASSIA_TREE_PLACED);
+                bootstrap.register(WGFeature.WG_MED_PLACED_KEY, WGFeature.WG_MED_PLACED);
+            }).add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, bootstrap -> {
+                HolderGetter<Biome> biomes = bootstrap.lookup(Registries.BIOME);
+                HolderGetter<PlacedFeature> placedFeatures = bootstrap.lookup(Registries.PLACED_FEATURE);
+                bootstrap.register(WGBiomeModifier.CASSIA_TREE, new BiomeModifiers.AddFeaturesBiomeModifier(
+                        HolderSet.direct(List.of(biomes.getOrThrow(Biomes.DARK_FOREST), biomes.getOrThrow(Biomes.FOREST), biomes.getOrThrow(Biomes.BIRCH_FOREST))),
+                        HolderSet.direct(placedFeatures.getOrThrow(WGFeature.CASSIA_TREE_PLACED_KEY)), GenerationStep.Decoration.SURFACE_STRUCTURES));
+                bootstrap.register(WGBiomeModifier.WG_MED, new BiomeModifiers.AddFeaturesBiomeModifier(
+                        HolderSet.direct(List.of(biomes.getOrThrow(Biomes.DARK_FOREST), biomes.getOrThrow(Biomes.FOREST), biomes.getOrThrow(Biomes.BIRCH_FOREST))),
+                        HolderSet.direct(placedFeatures.getOrThrow(WGFeature.WG_MED_PLACED_KEY)), GenerationStep.Decoration.VEGETAL_DECORATION));
+            }), Set.of(WhisperGrove.MODID));
     }
 
 }
